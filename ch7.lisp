5bbf12d4c42483a17039af475da14b4ee4974257
Niranjan <bajgainiranjan@gmail.com>
2021-07-09 13:03:35 -0600

    finish page 288

--- ch7.lisp
+++ ch7.lisp
@@ -36,3 +36,32 @@
 (defun my-assoc (key table)
   (find-if #'(lambda (entry)
 	       (equal key (first entry))) table))
+
+;;;7.8
+(defun roughly-equal (x k)
+  (find-if #'(lambda (i)
+	       (and (> i (- k 10)) (< i (+ k 10)))) x))
+
+;;;7.9
+(defun find-nested (x)
+  (find-if #'consp x))
+
+;;; 7.10
+(defvar note-table)
+(setf note-table
+      '((c 1)
+	(c-sharp 2)
+	(d 3)
+	(d-sharp 4)
+	(e 5)
+	(f 6)
+	(f-sharp 7)
+	(g 8)
+	(g-sharp 9)
+	(a 10)
+	(a-shrap 11)
+	(b 12)))
+
+(defun numbers (notes)
+  (mapcar #'(lambda (n) (second (assoc n note-table))) notes))
+
