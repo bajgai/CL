8c0f80a7adb2ff3b9f34778e0def9948c9c305f8
Niranjan Bajgai <niranjanbajgai@Niranjans-Mac-mini.local>
2021-10-07 10:53:15 -0600

    random commit

--- ch7.lisp
+++ ch7.lisp
@@ -1,6 +1,5 @@
 
-(defun square (n) (* n n))
-
+;;;example
 (defvar words)
 (setf words
       '((one un)
@@ -9,59 +8,33 @@
 	(four quatre)
 	(five cinq)))
 
-(defun translate (x)
-  (second (assoc x words)))
 
-;;;Exercise 7.1
+;;; Exercise 7.1
+
 (defun add1 (x)
   (+ x 1))
+(mapcar #'add1 '(13 5 7 9))
 
-;;;Exercise 7.2
+
+;;; Exercise 7.2
+
 (defvar daily-planet)
+
 (setf daily-planet
       '((olsen jimmy 123-76-4535 cub-reporter)
-        (kent clark 089-52-6787 reporter)
-        (lane lois 951-26-1438 reporter)
-        (white perry 355-16-7439 editor)))
+	(kent clark 089-52-6787 reporter)
+	(lane lois 951-25-1438 reporter)
+	(white perry 355-16-7439 editor)))
 
-;;;Exercise 7.4
+;;; Exercise 7.4
+
 (defun grater-than-five-p (x)
-  (if (> x 5) t))
+  (if (> x 5) t nil))
+  
+;;;Exercise 7.8
 
-;;;Exercise 7.7
-(defun testfunc (x)
-  (mapcar #'(lambda (n) (if (equal n 'up) 'down 'up)) x))
+(mapcar #'(lambda (x) (if (equal x 'up) 'down 'up)) '(up down up down))
 
-;;;Example
-(defun my-assoc (key table)
-  (find-if #'(lambda (entry)
-	       (equal key (first entry))) table))
-
-;;;7.8
-(defun roughly-equal (x k)
-  (find-if #'(lambda (i)
-	       (and (> i (- k 10)) (< i (+ k 10)))) x))
-
-;;;7.9
-(defun find-nested (x)
-  (find-if #'consp x))
-
-;;; 7.10
-(defvar note-table)
-(setf note-table
-      '((c 1)
-	(c-sharp 2)
-	(d 3)
-	(d-sharp 4)
-	(e 5)
-	(f 6)
-	(f-sharp 7)
-	(g 8)
-	(g-sharp 9)
-	(a 10)
-	(a-shrap 11)
-	(b 12)))
-
-(defun numbers (notes)
-  (mapcar #'(lambda (n) (second (assoc n note-table))) notes))
-
+;;; example
+(defun my-assoc(key table)
+  (find-if #'(lambda (entry) (equal key (first entry))) table))
---
+++ sbcl-2.1.2-arm64-darwin/src/runtime/sbcl.mk
@@ -0,0 +1,8 @@
+CC=cc
+LD=ld
+CFLAGS=-g -Wall -Wundef -Wsign-compare -Wpointer-arith -O3 -g -Wall -O2 -fdollars-in-identifiers -fno-omit-frame-pointer
+ASFLAGS=-g -Wall -Wundef -Wsign-compare -Wpointer-arith -O3 -g -Wall -O2 -fdollars-in-identifiers -fno-omit-frame-pointer
+LINKFLAGS=-g -dynamic -twolevel_namespace -bind_at_load
+LDFLAGS=
+__LDFLAGS__=
+LIBS=-lSystem -lc -ldl -lpthread  -lm
