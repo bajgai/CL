62fd4fedaca4e9149dc2baa27cc5f1198977b63d
Niranjan Bajgai <bajgainiranjan@gmail.com>
2021-10-11 10:07:45 -0600

    complete ex 7.9

--- ch7.lisp
+++ ch7.lisp
@@ -31,10 +31,36 @@
 (defun grater-than-five-p (x)
   (if (> x 5) t nil))
   
-;;;Exercise 7.8
+;;;Exercise 7.7
 
 (mapcar #'(lambda (x) (if (equal x 'up) 'down 'up)) '(up down up down))
 
 ;;; example
 (defun my-assoc(key table)
   (find-if #'(lambda (entry) (equal key (first entry))) table))
+
+;;;exercise 7.8
+
+(defun roughly-equal (x k)
+  (find-if #'(lambda (entry) (and (> entry (- k 10)) (< entry (+ k 10)))) x))
+
+;;;exercise 7.9
+(defun find-nested (x)
+  (find-if #'consp x))
+
+;;;exercise 7.10
+
+(defvar note-table)
+(setf note-table
+      '(('c 1)
+	('c-sharp 2)
+	('d 3)
+	('d-sharp 4)
+	('e 5)
+	('f 6)
+	('f-sharp 7)
+	('g 8)
+	('g-sharp 9)
+	('a 10)
+	('a-sharp 11)
+	('b 12)))
